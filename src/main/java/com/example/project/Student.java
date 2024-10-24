960757684a3d7ef2b3726388038b3472f9392185
Casey Walker <caseyw16@nycstudents.net>
2024-10-24 13:02:42 +0000

    commit

--- src/main/java/com/example/project/Student.java
+++ src/main/java/com/example/project/Student.java
@@ -2,81 +2,82 @@ package com.example.project;
 
 public class Student {
     // instance variables for 
-    // first name (String)
-    // last name (String)
-    // graduation year (int)
-    // sum of test scores (double).. should initialize at 0.0
-    // test score count (int) ..should initialize at 0
-    // highest test score (double).. should initialize at 0.0
+    private String firstName;
+    private String lastName;
+    private int gradYear;
+    private double testScoreSum;
+    private int numTestScores;
+    private double maxTestScore;
  
     // constructor
     public Student(String firstName, String lastName, int gradYear) {
         //implement code here!
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradYear = gradYear;
 
+        this.testScoreSum = 0.0;
+        this.numTestScores = 0;
+        this.maxTestScore = 0.0;
     }
  
-    // returns firstName
     public String getFirstName() {
-        //implement code here!
-        return "";
+        return firstName;
     }
  
-    // returns lastName
     public String getLastName() {
-        //implement code here!
-        return "";
+        return lastName;
     }
  
     public double getHighestTestScore() {
-        //implement code here!
-        return 0.0;
+        return maxTestScore;
     }
 
     public int getTestScoreCount(){
-        //implement code here!
-        return 0;
+        return numTestScores;
     }
 
     public int getGradYear(){
-        //implement code here!
-        return 0;
+        return gradYear;
     }
  
     // sets gradYear to newGradYear
     public void setGradYear(int newGradYear) {
-        //implement code here!
+        gradYear = newGradYear;
     }
  
     // adds newTestScore to accumulatedTestScores
     // and increments testScoreCount by 1
     // set new highest test score 
     public void addTestScore(double newTestScore) {
-        //implement code here!
+        testScoreSum += newTestScore;
+        numTestScores++;
+        if (newTestScore > maxTestScore) {
+            maxTestScore = newTestScore;
+        }
     }
  
     // returns true if the student's average test score is greater
     // than or equal to 65; returns false otherwise (see Note 2 below)
     public boolean isPassing() {
-        //implement code here!
-        return false;
+        return averageTestScore() > 65;
     }
  
     // returns the Student's average test score as the
     // quotient of accumulatedTestScores and testScoreCount
     public double averageTestScore() {
-        //implement code here!
-        return 0.0;
+        return testScoreSum / numTestScores;
     }
  
     // this method prints all info of a Student object to the console 
     // I AM NOT TESTING YOU ON THIS METHOD. IT'S FOR YOUR TESTING PURPOSES ONLY.. you don't have to use it
     public void printStudentInfo() {
-        System.out.println("Student Full Name: " );
-        System.out.println("Graduation Year: ");
-        System.out.println("Number of tests: ");
-        System.out.println("Average Test Score: ");
-        System.out.println("Highest Test Score: ");
-        System.out.println("Is passing: ");
+        System.out.println("Student Full Name: " + firstName + " " + lastName);
+        System.out.println("Graduation Year: " + gradYear);
+        System.out.println("Number of tests: " + numTestScores);
+        System.out.println("Average Test Score: " + averageTestScore());
+        System.out.println("Highest Test Score: " + maxTestScore);
+        System.out.println("Is passing: " + (isPassing() ? "yes" : "no"));
     }
  }
  
--- src/main/java/com/example/project/StudentRunner.java
+++ src/main/java/com/example/project/StudentRunner.java
@@ -1,5 +1,11 @@
+package com.example.project;
+
 public class StudentRunner{
     public static void main(String[] args) {
-        //use this StudentRunner class if you need to test your code 
+        Student guy = new Student("Person", "Human", 3052);
+        guy.addTestScore(80);
+        guy.addTestScore(100);
+        guy.addTestScore(95);
+        guy.printStudentInfo();
     }
 }
